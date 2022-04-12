4c7de6bb912fdc440edca20ef808514489f53572
Geison Amorim <geisonam43@gmail.com>
2022-04-12 12:09:12 -0300

    Add project files.

---
+++ ValidarSenha/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variavel
+            int senha;
+
+            //Entrada 
+            senha = int.Parse(Console.ReadLine());
+
+            //processamento / saida
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida!");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso Permitido!");
+        }
+    }
+}
